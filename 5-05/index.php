11cbb468a4fc28dfc5b6d86ab0b23f42824d7193
jpennyiii <201880068@psu.palawan.edu.ph>
2022-09-19 22:11:27 +0800

    5-5 commit

--- 5-05/index.php
+++ 5-05/index.php
@@ -1,5 +1,19 @@
 <?php
-
+	$num_visits;
+	session_start();
+	if(isset($_SESSION['views'])){
+    $_SESSION['views'] = $_SESSION['views']+1;
+	}
+	else{
+    $_SESSION['views']=1;
+	}
+	$num_visits=$_SESSION['views'];
+	if($num_visits==5){
+		echo "<h4>Thank you for visiting the page a lot.</h4>";
+	}if($num_visits==10){
+		header('Location:congratulations.php');
+	}
+	
 ?>
 
 <h1>Hello</h1>
